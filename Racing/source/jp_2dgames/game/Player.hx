32d0043f46167338d27a55f9831dc5c9f784c5ac
syun77 <2dgames.jp@gmail.com>
2015-09-20 11:32:05 +0900

    fixed #9 FlixelGames-9 壁に当たったら止まるようにする

--- Racing/source/jp_2dgames/game/Player.hx
+++ Racing/source/jp_2dgames/game/Player.hx
@@ -9,6 +9,18 @@ class Player extends FlxSprite {
 
   static inline var DECAY_ROLL = 0.01;
 
+  public var left(get, never):Float;
+  private function get_left() {
+    return x;
+  }
+  public var right(get, never):Float;
+  private function get_right() {
+    return x + width;
+  }
+
+  /**
+   * コンストラクタ
+   **/
   public function new(X:Float, Y:Float) {
     super(X, Y);
     loadGraphic(Reg.PATH_IMAGE_CAR_RED);
@@ -18,6 +30,9 @@ class Player extends FlxSprite {
     velocity.y = -100;
   }
 
+  /**
+   * 旋回する
+   **/
   public function roll(d:Float):Void {
     d *= DECAY_ROLL;
     var vx = velocity.x;
@@ -29,5 +44,11 @@ class Player extends FlxSprite {
     var radian = angle * FlxAngle.TO_RAD;
     velocity.x = speed * Math.cos(radian);
     velocity.y = speed * Math.sin(radian);
+  }
+
+  override public function update():Void {
+    super.update();
+
+    Wall.clip(this);
   }
 }
---
+++ Racing/source/jp_2dgames/game/Wall.hx
@@ -0,0 +1,31 @@
+package jp_2dgames.game;
+
+/**
+ * 壁
+ **/
+class Wall {
+
+  static inline var LEFT = 44;
+  static inline var RIGHT = 192;
+
+  /**
+   * 道路に収まるようにクリッピングする
+   **/
+  public static function clip(spr:Player):Bool {
+
+    var ret = false;
+
+    if(LEFT > spr.left) {
+      // 左カベに衝突
+      spr.x = LEFT;
+      ret = true;
+    }
+    if(RIGHT < spr.right) {
+      // 右カベに衝突
+      spr.x = RIGHT - spr.width;
+      ret = true;
+    }
+
+    return ret;
+  }
+}
