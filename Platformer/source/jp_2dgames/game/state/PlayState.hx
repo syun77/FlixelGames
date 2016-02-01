45450a6dfec3e5aeb777b14ee93ad7f2f8f2dc87
syun77 <kenmomomomomo@gmail.com>
2016-02-01 11:36:08 +0900

    fixed #34 FlixelGames-34 マップの表示

--- Platformer/source/jp_2dgames/game/state/PlayState.hx
+++ Platformer/source/jp_2dgames/game/state/PlayState.hx
@@ -1,10 +1,15 @@
 package jp_2dgames.game.state;
 
+import flash.display.BitmapData;
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
 import flixel.tile.FlxTilemap;
 import jp_2dgames.lib.TmxLoader;
 import flixel.FlxState;
+
+// オートタイル画像
+@:bitmap("assets/data/autotiles.png")
+class TileGraphic extends BitmapData {}
 
 /**
  * メインゲーム
@@ -13,15 +18,15 @@ class PlayState extends FlxState {
   override public function create():Void {
     super.create();
 
-    this.add(new FlxSprite(0, 0).makeGraphic(16, 16, FlxColor.RED));
-
     // マップ読み込み
     var tmx = new TmxLoader();
     tmx.load("assets/data/001.tmx");
     var csv = tmx.getLayerCsv(0);
     var map = new FlxTilemap();
-    map.loadMap(csv, GraphicAuto, 0, 0, FlxTilemap.AUTO);
+    map.loadMap(csv, TileGraphic, 0, 0, FlxTilemap.AUTO);
     this.add(map);
+
+    this.add(new FlxSprite(0, 0).makeGraphic(16, 16, FlxColor.RED));
   }
 
   override public function destroy():Void {
