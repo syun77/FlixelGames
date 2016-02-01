7804915cbc5c780ce40705ccf1d6c3de32b1db14
syun77 <kenmomomomomo@gmail.com>
2016-02-01 10:48:25 +0900

    fixed #31 FlixelGames-31 マップ作成

--- Platformer/source/jp_2dgames/game/state/PlayState.hx
+++ Platformer/source/jp_2dgames/game/state/PlayState.hx
@@ -1,5 +1,6 @@
 package jp_2dgames.game.state;
 
+import jp_2dgames.lib.TmxLoader;
 import flixel.FlxState;
 
 /**
@@ -8,6 +9,12 @@ import flixel.FlxState;
 class PlayState extends FlxState {
   override public function create():Void {
     super.create();
+
+    // マップ読み込み
+    var tmx = new TmxLoader();
+    tmx.load("assets/data/001.tmx");
+    var layer = tmx.getLayer(0);
+    layer.dump();
   }
 
   override public function destroy():Void {
