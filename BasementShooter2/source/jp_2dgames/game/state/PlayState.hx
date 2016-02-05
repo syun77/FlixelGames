10ffe3987096b76f6fb7383fc8314e608379f3c3
syun77 <kenmomomomomo@gmail.com>
2016-02-05 21:20:40 +0900

    fixed #84 FlixelGames-84 マップスクロールの実装

--- BasementShooter2/source/jp_2dgames/game/state/PlayState.hx
+++ BasementShooter2/source/jp_2dgames/game/state/PlayState.hx
@@ -1,19 +1,25 @@
 package jp_2dgames.game.state;
 
+import flixel.FlxCamera;
 import jp_2dgames.game.global.Global;
 import jp_2dgames.game.util.Field;
 import flixel.FlxG;
 import jp_2dgames.game.token.Player;
 import flash.display.BitmapData;
 import flixel.tile.FlxTilemap;
-import jp_2dgames.lib.TmxLoader;
 import flixel.FlxState;
 
+/**
+ * メインゲーム画面
+ **/
 class PlayState extends FlxState {
 
   var _map:FlxTilemap;
   var _player:Player;
 
+  /**
+   * 生成
+   **/
   override public function create():Void {
     super.create();
 
@@ -24,8 +30,16 @@ class PlayState extends FlxState {
     _player = new Player(32, 32);
     this.add(_player.getLight());
     this.add(_player);
+
+    // カメラ設定
+    FlxG.camera.follow(_player, FlxCamera.STYLE_PLATFORMER);
+    FlxG.worldBounds.set(0, 0, Field.getWidth(), Field.getHeight());
+
   }
 
+  /**
+   * 破棄
+   **/
   override public function destroy():Void {
 
     Field.unload();
@@ -33,6 +47,9 @@ class PlayState extends FlxState {
     super.destroy();
   }
 
+  /**
+   * 更新
+   **/
   override public function update():Void {
     super.update();
 
