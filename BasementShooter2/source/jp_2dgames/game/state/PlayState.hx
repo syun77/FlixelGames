bfee35637acf3fc0dca35695ee38f9b76b8f03e6
syun77 <kenmomomomomo@gmail.com>
2016-02-05 11:09:47 +0900

    fixed #77 FlixelGames-77 マップの読み込みと表示

--- BasementShooter2/source/jp_2dgames/game/state/PlayState.hx
+++ BasementShooter2/source/jp_2dgames/game/state/PlayState.hx
@@ -1,10 +1,24 @@
 package jp_2dgames.game.state;
 
+import flash.display.BitmapData;
+import flixel.tile.FlxTilemap;
+import jp_2dgames.lib.TmxLoader;
 import flixel.FlxState;
+
+// オートタイル画像
+@:bitmap("assets/data/autotiles.png")
+class TileGraphic extends BitmapData {}
 
 class PlayState extends FlxState {
   override public function create():Void {
     super.create();
+
+    var tmx = new TmxLoader();
+    tmx.load("assets/data/001.tmx");
+    var csv = tmx.getLayerCsv("object");
+    var map = new FlxTilemap();
+    map.loadMap(csv, TileGraphic, 0, 0, FlxTilemap.AUTO);
+    this.add(map);
   }
 
   override public function destroy():Void {
