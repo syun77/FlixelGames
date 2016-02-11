cdceb87bef343c2be4e8482df54fbbb1e330f085
syun77 <kenmomomomomo@gmail.com>
2016-02-11 23:46:24 +0900

    fixed #135 FlixelGames-135 Libを登録

---
+++ Puzzle/source/jp_2dgames/game/state/PlayState.hx
@@ -0,0 +1,46 @@
+package jp_2dgames.game.state;
+
+import flixel.FlxG;
+import flixel.FlxState;
+
+/**
+ * メインゲーム画面
+ **/
+class PlayState extends FlxState {
+
+  /**
+   * 生成
+   **/
+  override public function create():Void {
+    super.create();
+
+  }
+
+  /**
+   * 破棄
+   **/
+  override public function destroy():Void {
+    super.destroy();
+  }
+
+  /**
+   * 更新
+   **/
+  override public function update():Void {
+    super.update();
+    #if neko
+    _updateDebug();
+    #end
+  }
+
+  function _updateDebug():Void {
+    if(FlxG.keys.justPressed.ESCAPE) {
+      // 強制終了
+      throw "Terminate.";
+    }
+    if(FlxG.keys.justPressed.L) {
+      // リスタート
+      FlxG.resetState();
+    }
+  }
+}
