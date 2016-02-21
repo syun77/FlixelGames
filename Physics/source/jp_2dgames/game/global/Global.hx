437e6b1fef3aaaa3ca44b3fda6c7339997815ee0
syun77 <2dgames.jp@gmail.com>
2016-02-21 21:19:10 +0900

    fixed #213 FlixelGames-213 ライフの実装

--- Physics/source/jp_2dgames/game/global/Global.hx
+++ Physics/source/jp_2dgames/game/global/Global.hx
@@ -2,7 +2,7 @@ package jp_2dgames.game.global;
 class Global {
 
   public static inline var MAX_LEVEL:Int = 6;
-  static inline var MAX_LIFE:Int = 0;
+  static inline var MAX_LIFE:Int = 3;
   static inline var START_LEVEL:Int = 1;
   static inline var MAX_SHOT:Float = 100.0;
 
