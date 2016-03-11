d704c4758bf348b1e00df44c8442ad78dd84fe7c
syun77 <2dgames.jp@gmail.com>
2016-03-11 22:10:52 +0900

    fixed #340 FlixelGames-340 ダメージ演出

--- ThreeMatch/source/jp_2dgames/game/global/Global.hx
+++ ThreeMatch/source/jp_2dgames/game/global/Global.hx
@@ -2,7 +2,7 @@ package jp_2dgames.game.global;
 class Global {
 
   static inline var MAX_LEVEL:Int = 4;
-  public static inline var MAX_LIFE:Int = 3;
+  public static inline var MAX_LIFE:Int = 100;
   static inline var START_LEVEL:Int = 1;
   static inline var MAX_SHOT:Float = 100.0;
   static inline var FIRST_MONEY:Int = 0;
