1824622ce7f0893bb8f01adaa11b480302fbab73
syun77 <2dgames.jp@gmail.com>
2016-04-03 22:12:07 +0900

    敵クラスの追加 #527

--- Reaction/source/jp_2dgames/game/token/Enemy.hx
+++ Reaction/source/jp_2dgames/game/token/Enemy.hx
@@ -43,18 +43,13 @@ class Enemy extends Token {
   public function init(eid:Int, X:Float, Y:Float, deg:Float, speed:Float):Void {
     _eid = eid;
 
-    var sprite_name = "nasu.png";
+    var sprite_name = "milk.png";
 
     // TexturePackerData is a helper class to store links to atlas image and atlas data files
     var tex = FlxAtlasFrames.fromTexturePackerJson("assets/images/enemy.png", "assets/images/enemy.json");
     frames = tex;
     animation.frameName = sprite_name;
-    var frame = tex.getByName(sprite_name);
-    _width = frame.frame.width;
-    _height = frame.frame.height;
-
-    x = X - _width/2;
-    y = Y - _height/2;
+    resetSizeFromFrame();
     setVelocity(deg, speed);
   }
 
@@ -62,13 +57,4 @@ class Enemy extends Token {
 
   // ------------------------------------------------------------
   // ■アクセサ
-  override public function get_xcenter() {
-    trace(super.get_xcenter());
-    return x + _width/2;
-  }
-
-  override public function get_ycenter() {
-    trace(super.get_ycenter());
-    return y + _height/2;
-  }
 }
