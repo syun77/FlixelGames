83d95df24513669ab40a0f2a0e8540ab779d8277
syun77 <2dgames.jp@gmail.com>
2016-04-03 19:02:08 +0900

    fixed #519 FlixelGames-519 Lib登録

---
+++ Reaction/source/jp_2dgames/game/gui/GameUI.hx
@@ -0,0 +1,60 @@
+package jp_2dgames.game.gui;
+
+import jp_2dgames.lib.StatusBar;
+import flixel.math.FlxPoint;
+import flixel.FlxG;
+import jp_2dgames.game.global.Global;
+import flixel.text.FlxText;
+import flixel.group.FlxSpriteGroup;
+
+/**
+ * ゲームUI
+ **/
+class GameUI extends FlxSpriteGroup {
+
+  static inline var FONT_SIZE:Int = 8;
+
+  // ---------------------------------------------------
+  // ■フィールド
+  var _txtLevel:FlxText;
+  var _txtScore:FlxText;
+  var _barHorming:StatusBar;
+
+  /**
+   * コンストラクタ
+   **/
+  public function new() {
+    super(4, 2);
+
+    var px:Float = 0;
+    var py:Float = 0;
+
+    // スコア
+    _txtScore = new FlxText(px, py, 0, "", FONT_SIZE);
+    this.add(_txtScore);
+
+    // レベル
+    _txtLevel = new FlxText(px, py+12, 0, "", FONT_SIZE);
+    this.add(_txtLevel);
+
+    // ホーミングゲージ
+    px += FlxG.width * 0.25;
+    py += 2;
+    _barHorming = new StatusBar(px, py, 128, 8, true);
+    this.add(_barHorming);
+
+    scrollFactor.set();
+  }
+
+  /**
+   * 更新
+   **/
+  public override function update(elapsed:Float):Void {
+    super.update(elapsed);
+
+    _txtScore.text = 'SCORE: ${Global.score}';
+    _txtLevel.text = 'LEVEL: ${Global.level}';
+    _barHorming.setPercent(Global.shot);
+
+  }
+}
