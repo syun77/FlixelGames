0cba890758dfe60aefb997313c3097dd30846b76
syun77 <2dgames.jp@gmail.com>
2016-04-03 20:37:47 +0900

    fixed #526 FlixelGames-526 ショットの反動を実装

--- Reaction/source/jp_2dgames/game/token/Player.hx
+++ Reaction/source/jp_2dgames/game/token/Player.hx
@@ -1,10 +1,11 @@
 package jp_2dgames.game.token;
 
+import flixel.FlxG;
+import jp_2dgames.lib.Input;
+import jp_2dgames.lib.MyMath;
 /**
  * アニメ定数
  **/
-import jp_2dgames.lib.Input;
-import jp_2dgames.lib.MyMath;
 private enum Anim {
   Standby; // 待機中
   Danger;  // 危険
@@ -16,6 +17,9 @@ private enum Anim {
 class Player extends Token {
 
   static inline var RADIUS:Float = 12.0;
+  static inline var REACT_SPEED:Float = 100.0;
+  static inline var MAX_SPEED:Float = 500.0;
+  static inline var DRAG_SPEED:Float = 100.0;
 
   // ----------------------------------
   // ■フィールド
@@ -31,6 +35,9 @@ class Player extends Token {
     loadGraphic(AssetPaths.IMAGE_PLAYER, true);
     _registerAnim();
     _playAnim();
+
+    maxVelocity.set(MAX_SPEED, MAX_SPEED);
+    drag.set(DRAG_SPEED, DRAG_SPEED);
 
     _cursor = cursor;
     _rot = 0;
@@ -49,6 +56,9 @@ class Player extends Token {
 
     // 回転
     _rotate();
+
+    // カベとの衝突で反動する
+    _reflectWall();
   }
 
   /**
@@ -59,6 +69,10 @@ class Player extends Token {
     var dy = _cursor.ycenter - ycenter;
     var rot = MyMath.atan2Ex(-dy, dx);
     Shot.add(xcenter, ycenter, rot, 200);
+
+    // 反動
+    velocity.x -= REACT_SPEED * MyMath.cosEx(rot);
+    velocity.y -= REACT_SPEED * -MyMath.sinEx(rot);
   }
 
   /**
@@ -71,6 +85,33 @@ class Player extends Token {
     var dRot = MyMath.deltaAngle(_rot, rot);
     _rot += dRot * 0.1;
     angle = 360 - _rot;
+  }
+
+  /**
+   * カベとの衝突の反動
+   **/
+  function _reflectWall():Void {
+    var x1 = 0;
+    var y1 = 0;
+    var x2 = FlxG.width - width;
+    var y2 = FlxG.height - height;
+
+    if(x < x1) {
+      x = x1;
+      velocity.x *= -1;
+    }
+    if(y < y1) {
+      y = y1;
+      velocity.y *= -1;
+    }
+    if(x > x2) {
+      x = x2;
+      velocity.x *= -1;
+    }
+    if(y > y2) {
+      y = y2;
+      velocity.y *= -1;
+    }
   }
 
   /**
