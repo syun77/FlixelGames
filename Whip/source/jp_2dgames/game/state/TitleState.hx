e8dca90e885bebec9020a4b5f0e6fe470fddf2f5
syun77 <2dgames.jp@gmail.com>
2016-04-19 18:09:30 +0900

    fixed #642 FlixelGames-642 ロープ画像の作成

--- Whip/source/jp_2dgames/game/state/TitleState.hx
+++ Whip/source/jp_2dgames/game/state/TitleState.hx
@@ -11,6 +11,9 @@ import flixel.FlxState;
  * タイトル画面
  **/
 class TitleState extends FlxState {
+
+  static inline var FONT_SIZE:Int = 8 * 3;
+
   override public function create():Void
   {
     super.create();
@@ -18,8 +21,8 @@ class TitleState extends FlxState {
     var bg = new FlxSprite(0, FlxG.height*0.2);
     bg.makeGraphic(FlxG.width, 32, FlxColor.GRAY);
     this.add(bg);
-    var txt = new FlxText(0, FlxG.height*0.2, FlxG.width, "WIRE ACTION");
-    txt.setFormat(null, 24, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, 0xFF3c3c3c);
+    var txt = new FlxText(0, FlxG.height*0.2, FlxG.width, "ROPE ACTION");
+    txt.setFormat(null, FONT_SIZE, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, 0xFF3c3c3c);
     this.add(txt);
     new FlxTimer().start(3, function(timer:FlxTimer) {
       var w = FlxG.random.float(0.005, 0.01);
@@ -35,7 +38,7 @@ class TitleState extends FlxState {
     btn.x -= btn.width/2;
     this.add(btn);
 
-    var txtCopy = new FlxText(0, FlxG.height-32, FlxG.width, "(C) 2016 2dgames.jp");
+    var txtCopy = new FlxText(0, FlxG.height-32, FlxG.width, "(C) 2016 2dgames.jp", 8);
     txtCopy.alignment = "center";
     this.add(txtCopy);
   }
