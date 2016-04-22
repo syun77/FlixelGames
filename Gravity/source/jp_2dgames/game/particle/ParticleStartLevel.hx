b3ad2a0a5821ce258e620ddb59f6b8d4c9ca7650
syun77 <2dgames.jp@gmail.com>
2016-04-22 10:42:40 +0900

    fixed #663 FlixelGames-663 Lib登録

---
+++ Gravity/source/jp_2dgames/game/particle/ParticleStartLevel.hx
@@ -0,0 +1,43 @@
+package jp_2dgames.game.particle;
+
+import flixel.util.FlxDestroyUtil;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.FlxState;
+import jp_2dgames.game.global.Global;
+
+/**
+ * レベル開始演出
+ **/
+class ParticleStartLevel {
+
+  static inline var FONTSIZE:Int = 16;
+
+  public static function start(state:FlxState):Void {
+    // ステージ開始演出
+    var txt = new FlxText(0, FlxG.height*0.3, FlxG.width, 'LEVEL ${Global.level}');
+    if(Global.level == Global.MAX_LEVEL-1) {
+      txt.text = "FINAL LEVEL";
+    }
+
+//    txt.text = "READY";
+
+    txt.setFormat(null, FONTSIZE, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+    var px = txt.x;
+    txt.x = -FlxG.width*0.75;
+    FlxTween.tween(txt, {x:px}, 1, {ease:FlxEase.expoOut, onComplete:function(tween:FlxTween) {
+      var px2 = FlxG.width * 0.75;
+      FlxTween.tween(txt, {x:px2}, 1, {ease:FlxEase.expoIn, onComplete:function(tween:FlxTween) {
+        // おしまい
+        // 破棄する
+        state.remove(txt);
+        txt = FlxDestroyUtil.destroy(txt);
+      }});
+    }});
+    txt.scrollFactor.set();
+    state.add(txt);
+  }
+}
---
+++ Gravity/source/jp_2dgames/game/state/BootState.hx
@@ -0,0 +1,32 @@
+package jp_2dgames.game.state;
+import flixel.FlxG;
+import flixel.FlxState;
+
+/**
+ * 起動画面
+ **/
+class BootState extends FlxState {
+  override public function create():Void {
+    super.create();
+
+    FlxG.debugger.toggleKeys = ["ALT"];
+
+//    EnemyInfo.load();
+  }
+
+  override public function destroy():Void {
+    super.destroy();
+  }
+
+  override public function update(elapsed:Float):Void {
+    super.update(elapsed);
+
+    #if flash
+    FlxG.switchState(new TitleState());
+    #else
+    FlxG.switchState(new PlayInitState());
+//    FlxG.switchState(new TitleState());
+//    FlxG.switchState(new EndingState());
+    #end
+  }
+}
