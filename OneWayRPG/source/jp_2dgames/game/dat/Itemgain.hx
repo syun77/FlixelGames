d0b9f48db29f303bd4247c5c9b5d393d67f03920
syun77 <2dgames.jp@gmail.com>
2016-05-02 10:34:16 +0900

    fixed #765 FlixelGames-765 アイテム出現テーブルの作成

---
+++ OneWayRPG/source/jp_2dgames/game/dat/Itemgain.hx
@@ -0,0 +1,34 @@
+package jp_2dgames.game.dat;
+
+import jp_2dgames.game.dat.MyDB;
+
+/**
+ * アイテム出現テーブル
+ **/
+class ItemGain {
+  public static function get(level:Int):Itemgain {
+    for(info in MyDB.itemgain.all) {
+      if(level == info.floor) {
+        return info;
+      }
+    }
+
+    throw 'Error: Invalid level = ${level}';
+  }
+
+  /**
+   * アイテムを抽選する
+   **/
+  public static function lotItem(level):Int {
+    var gen = new LotteryGenerator();
+
+    var info = get(level);
+    gen.add(info.item1, info.ratio1);
+    gen.add(info.item2, info.ratio2);
+    gen.add(info.item3, info.ratio3);
+    gen.add(info.item4, info.ratio4);
+    gen.add(info.item5, info.ratio5);
+
+    return gen.exec();
+  }
+}
